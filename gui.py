0ce42f5b14907896f3af515e0bfa5c85df2eaea2
Andrzej <pi@raspberrypi>
2015-09-30 18:45:04 +0200

    keypad+gui

--- gui.py
+++ gui.py
@@ -2,6 +2,37 @@
 #-*- coding: iso-8859-1 -*-
 
 import Tkinter
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+MATRIX = [[1,2,3,'A'],
+          [4,5,6,'B'],
+          [7,8,9,'C'],
+          ['*',0,'*', 'D']]
+ROW = [17,27,22,10]
+COL = [25,8,7,1]
+for j in range(4):
+    GPIO.setup(COL[j], GPIO.OUT)
+    GPIO.output(COL[j],1)
+for i in range(4):
+    GPIO.setup(ROW[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
+def checkButton():
+	try:
+		while(True):
+			for j in range(4):
+				GPIO.output(COL[j],0)
+				for i in range(4):
+					if GPIO.input(ROW[i])==0:
+						return MATRIX[i][j]
+						time.sleep(0.2)
+						while(GPIO.input(ROW[i])==0):
+							pass
+				GPIO.output(COL[j],1)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+pressed = checkButton()
 
 class simpleapp(Tkinter.Tk):
     def __init__(self,parent):
@@ -10,20 +41,18 @@ class simpleapp(Tkinter.Tk):
         self.initialize()
 
     def initialize(self):
-        self.grid()
-
-        self.entryVariable = Tkinter.StringVar()
+	self.grid()
+	self.entryVariable = Tkinter.StringVar()
         self.entry = Tkinter.Entry(self, textvariable=self.entryVariable)
         self.entry.grid(column=0,row=0,sticky='EW')
         self.entry.bind("<Return>", self.OnPressEnter)
         self.entryVariable.set(u"Enter text here")
         button = Tkinter.Button(self,text=u"Click",command=self.OnButtonClick)
         button.grid(column=1,row=0)
-
-        self.labelVariable = Tkinter.StringVar()
+	self.labelVariable = Tkinter.StringVar()
         label=Tkinter.Label(self,textvariable=self.labelVariable,anchor="w",fg="white",bg="blue")
         label.grid(column=0,row=1,columnspan=2, sticky='EW')
-        self.labelVariable.set(u"hello")
+        self.labelVariable.set(pressed)
 
         self.grid_columnconfigure(0,weight=1)
         self.resizable(True,False)
