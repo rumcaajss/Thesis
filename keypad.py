6cc8132a176bdc5b95c4b7d9b9bb1c0c801b31cf
Andrzej <pi@raspberrypi>
2015-09-30 18:21:45 +0200

    keypad_function

--- keypad.py
+++ keypad.py
@@ -8,27 +8,27 @@ MATRIX = [[1,2,3,'A'],
           ['*',0,'*', 'D']]
 ROW = [17,27,22,10]
 COL = [25,8,7,1]
-print MATRIX[2]
 for j in range(4):
     GPIO.setup(COL[j], GPIO.OUT)
     GPIO.output(COL[j],1)
 for i in range(4):
     GPIO.setup(ROW[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-try:
-    while(True):
-        for j in range(4):
-            GPIO.output(COL[j],0)
-            for i in range(4):
-                if GPIO.input(ROW[i])==0:
-                    if (MATRIX[i][j]=='A'):
-                        print('siema')
-                    elif(MATRIX[i][j]=='B'):
-                        print('BENG!')
-                    else:
-                        print('lel')
-                    time.sleep(0.2)
-                    while(GPIO.input(ROW[i])==0):
-                        pass
-            GPIO.output(COL[j],1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+def checkButton():
+	try:
+		while(True):
+			for j in range(4):
+				GPIO.output(COL[j],0)
+				for i in range(4):
+					if GPIO.input(ROW[i])==0:
+						return MATRIX[i][j]
+						time.sleep(0.2)
+						while(GPIO.input(ROW[i])==0):
+							pass
+				GPIO.output(COL[j],1)
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+pressed=checkButton()
+print(pressed)
+
+
