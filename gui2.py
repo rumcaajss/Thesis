8c39cd34aef4b967683b6285a9fc9983726b0cef
Andrzej <pi@raspberrypi>
2015-10-01 19:14:34 +0200

    second gui

--- gui2.py
+++ gui2.py
@@ -32,28 +32,39 @@ class StartPage(tk.Frame):
 		button.bind("<Return>",lambda event: controller.show_frame(PageOne))
 		button.pack()
 		button.focus_set()
-		button2 = tk.Button(self, text="Pressto start brewing")
+		button2 = tk.Button(self, text="Press to start brewing")
 		button2.bind("<Return>",lambda event: controller.show_frame(PageTwo))
 		button2.pack()
  
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Mashing", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-        self.rest_var = tk.StringVar()
-	self.rest_no= tk.Entry(self, textvariable=self.rest_var)
-	self.rest_no.pack()
-	self.rest_no.focus()
-	self.rest_no.bind("<Return>", self.ConvertToNumber)
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Mashing", font=LARGE_FONT)
+		label.pack(pady=10,padx=10)
+		label2=tk.Label(self, text="First rest temperature")
+		label2.pack()
+		self.temp_var = tk.IntVar()
+		self.temp1= tk.Entry(self, textvariable=self.temp_var)
+		self.temp1.pack()
+		self.temp1.focus()			
+		self.temp1.bind("<Return>", self.FirstTemp)
+		label3=tk.Label(self, text="First rest time")
+		label3.pack()
+		self.time_var = tk.IntVar()
+		self.time1= tk.Entry(self, textvariable=self.time_var)
+		self.time1.pack()
+		self.time1.focus()			
+		self.time1.bind("<Return>", self.FirstTime)
+		button1 = tk.Button(self, text="Back to Home")
+		button1.bind("<Return>",lambda event: controller.show_frame(StartPage))
+		button1.pack()
+    def FirstTemp(self, temp_var):
+		temp_var = self.temp_var.get()+2
+		self.temp_var.set(temp_var)
 	
-	
-        button1 = tk.Button(self, text="Back to Home")
-        button1.bind("<Return>",lambda event: controller.show_frame(StartPage))
-        button1.pack()
-    def ConvertToNumber(self,rest_var):
-		number = int(float(self.rest_var.get()))
-		print (number +2)
+    def FirstTime(self, time_var):
+        time_var=self.time_var.get()+2
+        self.time_var.set(time_var)
 
 class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
