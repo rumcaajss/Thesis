adbc5b2358f25166e264dd1b9457cbc38f7d9e8a
Andrzej <pi@raspberrypi>
2015-10-07 12:16:18 +0200

    optimalization

--- gui2.py
+++ gui2.py
@@ -20,23 +20,34 @@ class Sensor():
 class Start():
     def __init__(self, master, *args, **kwargs):
         #__init__(self, *args, **kwargs)
-        container = Frame(master, width=400,height=400)
-        container.pack(side="top", fill="both", expand = True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-        self.frames = {}
-
-        for F in (StartPage, PageOne, PageTwo, Mashing):
-			frame = F(container, self)		
-			self.frames[F] = frame
-			frame.grid(row=0, column=0, sticky="nsew")
-			self.show_frame(StartPage)
+		self.container = Frame(master, width=400,height=400)
+		self.container.pack(side="top", fill="both", expand = True)
+		self.container.grid_rowconfigure(0, weight=1)
+		self.container.grid_columnconfigure(0, weight=1)
+		self.history = []
+		self.show_frame(StartPage)
+        #for F in (StartPage, PageOne, PageTwo):
+		#	frame = F(container, self)		
+		#	self.frames[F] = frame
+		#	frame.grid(row=0, column=0, sticky="nsew")
+		#	self.show_frame(StartPage)
 			
-			
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-
+    def show_frame(self, cont):		
+		frame=cont(self.container, self)
+		frame.grid(row=0, column=0, sticky="nsew")
+        #frame = self.frames[cont]
+		frame.tkraise()
+		self.history.append(frame)  #attempts to optimize by deleting frames
+		print self.history
+		if len(self.history)==3:
+			print self.history[1]
+			print self.history[0]
+			self.history[0]=self.history[1]
+			print self.history[0]
+			#self.history[1]=self.history[2]
+			self.history[0].grid_forget()
+			#self.history[1].destroy()
+			self.history.pop(1)
         
 class StartPage(Frame):
     def __init__(self, parent, controller):
@@ -93,12 +104,15 @@ class PageOne(Frame):
 		self.time2.bind("<Return>", self.SecondTime)
 		
 		button1 = Button(self, text="Start")
-		button1.bind("<Return>", self.Control)
+		button1.bind("<Return>", lambda event: controller.show_frame(Mashing))
 		button1.pack()
 		
 		button2 = Button(self, text="Back to Home")
 		button2.bind("<Return>",lambda event: controller.show_frame(StartPage))
 		button2.pack()
+		
+		
+			
     def FirstTemp(self, event):
 		global temperatura
 		temp_var = self.temp_var.get()
@@ -121,10 +135,38 @@ class PageOne(Frame):
 		time_var2=self.time_var2.get()+2
 		self.time_var2.set(time_var2)
 		event.widget.tk_focusNext().focus()
-    def Control(self, event):
-		self.controller.show_frame(Mashing)
-		obj=Sensor("28-0215021f66ff")	
-		temp_meas=obj.temp_sensor("28-0215021f66ff")
+
+#    def Control(self, event):
+#		self.controller.show_frame(Mashing)
+#		obj=Sensor("28-0215021f66ff")	
+#		temp_meas=obj.temp_sensor("28-0215021f66ff")
+#		temp_set=temperatura
+#		error = temp_set-temp_meas
+#		print temp_meas
+#		print temp_set
+#		print error
+#		if error < 10:
+#			GPIO.output(23,GPIO.HIGH)
+#		else: 
+#			GPIO.output(23,GPIO.LOW)  
+
+class Mashing(Frame):
+	def __init__(self, parent, controller):
+		Frame.__init__(self, parent)
+		self.temp=StringVar()
+		self.temp_info=Label(self, text="Current temperature of the mash tun:", font=LARGE_FONT)
+		self.temp_info.grid(row=0)
+		self.text = Label(self,text=self.temp, font=LARGE_FONT)
+		self.text.grid(row=0, column=2)
+		self.GetTemp()		
+		button = Button(self, text="Back to Home")
+		button.bind("<Return>", lambda event: controller.show_frame(StartPage))		
+		button.grid(row=3, column=1)
+	
+	def GetTemp(self):
+		obj2=Sensor("28-0215021f66ff")	
+		temp_meas=obj2.temp_sensor("28-0215021f66ff")
+		self.text.configure(text=temp_meas)
 		temp_set=temperatura
 		error = temp_set-temp_meas
 		print temp_meas
@@ -133,24 +175,7 @@ class PageOne(Frame):
 		if error < 10:
 			GPIO.output(23,GPIO.HIGH)
 		else: 
-			GPIO.output(23,GPIO.LOW)  
-
-class Mashing(Frame):
-	def __init__(self, parent, controller):
-		window=Frame.__init__(self, parent)
-		self.temp=StringVar()
-		self.text = Label(self,text=self.temp, font=LARGE_FONT)
-		self.text.pack(pady=10, padx=10) 
-		self.GetTemp()		
-		button = Button(self, text="Back to Home")
-		button.bind("<Return>", lambda event: controller.show_frame(StartPage))		
-		button.pack()
-	
-	def GetTemp(self):
-		print("siema")
-		obj2=Sensor("28-0215021f66ff")	
-		temp_meas=obj2.temp_sensor("28-0215021f66ff")
-		self.text.configure(text=temp_meas)
+			GPIO.output(23,GPIO.LOW) 
 		self._timer=root.after(500,self.GetTemp) 
 		
 		
@@ -165,29 +190,13 @@ class PageTwo(Frame):
 		button2 = Button(self, text="Back to Home")
 		button2.bind("<Return>", lambda event: controller.show_frame(StartPage))		
 		button2.pack()
-
-		
-def control():
-	obj=Sensor("28-0215021f66ff")	
-	temp_meas=obj.temp_sensor("28-0215021f66ff")
-	temp_set=temperatura
-	error = temp_set-temp_meas
-	global zmierzona_temp
-	zmierzona_temp=temp_meas
-	print temp_meas
-	print temp_set
-	print error
-	if error < 10:
-		GPIO.output(23,GPIO.HIGH)
-	else: 
-		GPIO.output(23,GPIO.LOW)
-	root.after(2000,control)		
+	
 root=Tk()
 app=Start(root)
 root.title("BrewWizard")
 #root.after(2000,control)
 #root.after(2000, GetTemp)
-root.update_idletasks()
+#root.update_idletasks()
 root.mainloop()
 
 
